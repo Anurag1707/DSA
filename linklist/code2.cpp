dc25d3ba2ad44b0338a746ff46b0e933d084d447
Anurag1707 <Anuragjhariya1707@gmail.com>
2025-10-13 13:58:47 +0530

    DSA

---
+++ linklist/code2.cpp
@@ -0,0 +1,79 @@
+# include <iostream>
+using namespace std;
+
+struct node{
+  int data;
+  node *next;
+};
+
+node *first , *temp , *ttemp ,*p;
+
+void init(){
+  first=temp=ttemp=NULL;
+
+}
+
+void create_first(){
+  first = new node;
+  first -> next = NULL;
+  cout<<"enter data for first node : ";
+  cin>>first->data;
+}
+
+void add_node(){
+  temp=first;
+  while(temp -> next !=NULL){
+    temp=temp -> next;
+  }
+  ttemp=new node;
+  ttemp -> next =NULL;
+  cin>>ttemp-> data;
+  temp -> next =ttemp;
+}
+
+void Add_after(int x,int y){
+    temp=first;
+    while(temp ->data!=x){
+        temp=temp ->next;
+    }
+    ttemp=temp->next;
+    p=new node;
+    p -> data=y;
+    p -> next=ttemp;
+    temp -> next= p;
+}
+
+void display(){
+  temp = first;
+  while(temp !=NULL){
+  cout<<temp -> data<<endl;
+  temp=temp->next;
+  }
+}
+
+
+int main(){
+  init();
+  create_first();
+  int flag=1;
+  char ans;
+  Add_after(30,60);
+  while(flag==1){
+    cout<<"do you want to continue : ";
+    cin>>ans;
+    
+    if(ans=='y'){
+      cout<<"enter next data";
+      add_node();
+    }
+    else{
+      flag = 0;
+      display();
+    }
+  }
+
+  return 0;
+}
+
+    
+
---
+++ linklist/code4.cpp
@@ -0,0 +1,85 @@
+# include <iostream>
+using namespace std;
+
+struct node{
+  int data;
+  node *next;
+};
+
+node *first , *temp , *ttemp ,*p;
+
+void init(){
+  first=temp=ttemp=NULL;
+
+}
+
+void create_first(){
+  first = new node;
+  first -> next = NULL;
+  cout<<"enter data for first node : ";
+  cin>>first->data;
+}
+
+void add_node(){
+  temp=first;
+  while(temp -> next !=NULL){
+    temp=temp -> next;
+  }
+  ttemp=new node;
+  ttemp -> next =NULL;
+  cin>>ttemp-> data;
+  temp -> next =ttemp;
+}
+
+void add_before_first(int x){
+    
+    temp=new node;
+    temp -> data=x;
+    temp -> next=first;
+    first=temp;
+}
+
+
+void display(){
+    add_before_first(50);
+    cout<<endl;
+  temp = first;
+  while(temp !=NULL){
+  cout<<temp -> data<<endl;
+  temp=temp->next;
+  }
+}
+
+
+int main(){
+  init();
+  create_first();
+//   int flag=1;
+//   char ans;
+//   Add_before(30,60);
+//   while(flag==1){
+//     cout<<"do you want to continue : ";
+//     cin>>ans;
+    
+//     if(ans=='y'){
+//       cout<<"enter next data";
+//       add_node();
+//     }
+//     else{
+//       flag = 0;
+//       display();
+//     }
+//   }
+add_node();
+add_node();
+add_node();
+add_node();
+
+add_before_first(50);
+display();
+
+  return 0;
+}
+
+    
+
