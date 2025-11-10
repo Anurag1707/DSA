38c5b375e9e2af5d4493285e7dba3a2086e54da8
Anurag1707 <Anuragjhariya1707@gmail.com>
2025-11-10 19:54:27 +0530

    .

---
+++ linklist/xDCLL/code5.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+struct DCLL
+{
+ int data;
+ DCLL *next, *prev;
+};
+DCLL *first, *temp, *ttemp, *p;
+void create_first()
+{
+    first =new DCLL;
+    first->prev=first->next=first;
+    cin>>first->data;
+}
+
+void add_node()
+{
+    temp=first->prev; // last node me pahuch sakte h
+    ttemp=new DCLL;
+    cin>>ttemp->data;
+    ttemp->next=first;
+    first->prev=ttemp;
+    temp->next=ttemp;
+    ttemp->prev=temp;
+}
+void swapLLS()
+{
+   
+    temp=first->prev;
+    ttemp=temp->prev;
+    p=ttemp->prev;
+    p->next=temp;
+    temp->prev=p;
+    temp->next=ttemp;
+    ttemp->prev=temp;
+    ttemp->next=first;
+
+}
+void display()
+{
+   temp=first;
+   do{
+    cout<<temp->data<<" ";
+    temp=temp->next;
+   }while (temp!=first); 
+}
+int main()
+{
+    create_first();
+    add_node();
+    add_node();
+    swapLLS();
+    display();
+}
+//swap last and last 2nd
