50c455b49dd629807cd899cbd366fab586e188f2
Anurag1707 <Anuragjhariya1707@gmail.com>
2026-02-25 13:46:46 +0530

    5

---
+++ practice/5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, i = 1;
+
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+
+    do {
+        printf("%d ", i);
+        i++;
+    } while(i <= n);
+
+    return 0;
+}
