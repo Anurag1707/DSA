25bbe6358c61c61636d26126a22094b690f0f894
Anurag1707 <Anuragjhariya1707@gmail.com>
2026-02-26 12:37:54 +0530

    6

---
+++ practice/6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i = 1, sum = 0;
+
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+
+    do {
+        sum = sum + i;
+        i++;
+    } while(i <= n);
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
